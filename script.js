9765c9522e5f9e40023c81fd1ea5efc880ef2c62
Caroline <ajiboyecaroline95@gmail.com>
2024-12-15 13:12:28 +0100

    hmmm

---
+++ script.js
@@ -0,0 +1,57 @@
+function btn(){
+    window.location.href = "calculator.html"
+}
+function one(){
+    display.innerHTML += `1`
+}
+function two(){
+    display.innerHTML += `2`
+}
+function three(){
+    display.innerHTML += `3`
+}
+function four(){
+    display.innerHTML += `4`
+}
+function five(){
+    display.innerHTML += `5`
+}
+function six(){
+    display.innerHTML += `6`
+}
+function seven(){
+    display.innerHTML += `7`
+}
+function eight(){
+    display.innerHTML += `8`
+}
+function nine(){
+    display.innerHTML += `9`
+}
+function zero(){
+    display.innerHTML += `0`
+}
+function twoz(){
+    display.innerHTML += `00`
+}
+function dot(){
+    display.innerHTML += `.`
+}
+function minus(){
+    display.innerHTML += `-`
+}
+function times(){
+    display.innerHTML += `x`
+}
+function divide(){
+    display.innerHTML += `/`
+}
+function plus(){
+    display.innerHTML += `+`
+}
+function enter(){
+    alert('fhgjh')
+}
+function clearDisplay(){
+    display.innerHTML = ''
+}
